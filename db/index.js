f9c03445dd7d905b36ca79f5f60ebe65036b94f3
Zachary Oliver <zachary.oliver@yahoo.com>
2022-10-02 14:27:56 -0700

    heroku

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/juicebox-dev");
+const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev");
 
 const getAllUsers = async () => {
   const { rows } = await client.query(
