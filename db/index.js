1152297b0887e9965994f3883bacbd94afaa80dd
Zachary Oliver <zachary.oliver@yahoo.com>
2022-10-02 14:59:36 -0700

    final

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev");
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 const getAllUsers = async () => {
   const { rows } = await client.query(
